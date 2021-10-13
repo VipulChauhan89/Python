33d423ff8798a91f4ae19ac70a7510e5d6db1ea8
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-10-13 22:16:10 +0530

    Add files via upload

---
+++ Python_program_to_perform_arithmetic _operations/Arithmetic.py
@@ -0,0 +1,13 @@
+print("Enter two numbers to perform Addition,Subtraction,Multiplication and Division")
+num1=int(input("Enter First Number :"))
+num2=int(input("Enter Second Number :"))
+result=num1+num2
+print("Addition :",result)
+result=num1-num2
+print("Subtraction :",result)
+result=num1*num2
+print("Multiplication :",result)
+result=num1/num2
+print("Floating point division :",result)
+result=num1//num2
+print("Integer Division :",result)
