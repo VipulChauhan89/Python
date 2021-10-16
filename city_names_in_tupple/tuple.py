f5696f5f7f4722781b8a5eb33ba4fc0752aed307
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-10-16 22:28:32 +0530

    Add files via upload

---
+++ city_names_in_tupple/tuple.py
@@ -0,0 +1,11 @@
+print("NAME- Vipul Chauhan \nSECTION- E \nSTUDENT ID- 20011016")
+
+t=tuple()
+n=int(input("Enter the number of cities you want to enter in the tuple : "))
+
+print("Enter the ",n," cities")
+for i in range(0,n):
+    ch=input()
+    t=t+(ch,)
+    
+print(t)
