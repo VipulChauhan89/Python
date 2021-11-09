36dcbee18c1f5cf3350ed047eb2285d04f7f2b4e
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-11-09 22:27:56 +0530

    Create miles.py

---
+++ Distance-from-kilometers-to-miles/miles.py
@@ -0,0 +1,6 @@
+#Taking the input distance in kilometers from the user
+kilometers=float(input("Enter th distance in kilometers:"))
+#conversion factor to convert distance kilometers in miles
+#1 kilometers is eual to 0.621371 miles
+miles=kilometers*0.621371
+print(kilometers,'kilometers is equal to',miles,'miles')
