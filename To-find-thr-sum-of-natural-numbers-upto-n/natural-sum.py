4922d2574464cf230fb1819b1bc5efdebfc8ee3b
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-11-11 21:33:37 +0530

    Create natural-sum.py

---
+++ To-find-thr-sum-of-natural-numbers-upto-n/natural-sum.py
@@ -0,0 +1,10 @@
+num=int(input("Enter the number uto which you want to find the sum : "))
+if num < 0:
+   print("Enter a positive number")
+else:
+    sum=0
+    print("The sum of natural numbers upto",num,"is =", end=" ")
+    while(num>0):
+        sum+=num
+        num-=1
+    print(sum)
