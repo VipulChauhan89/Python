4db6c57cc6d00a99041107b9eba2bd3e867dded8
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-02-06 14:48:31 +0530

    Create bill.py

---
+++ Bill-generation-of-mangoes/bill.py
@@ -0,0 +1,17 @@
+'''
+  Program to calculate the bill for mangoes 
+  If the customer is known for more than 3 years and the bill is more than 600 than discount of 12% is given
+  and if the customer is known for more than 3 years but the bill is less than 600 than discount of 7% is given
+'''
+
+def total_bill(a,b):
+      if(b>3 and a>600):
+         return (a*88)/100.0;
+      elif(b>3 and a<=600):
+         return (a*93)/100.0;
+      else:
+         return a;
+
+bill=float(input("Enter the bill amount : "))
+year=float(input("Enter the number of year he is known to the seller : "))
+print("The total bill is = {}\n".format(float(total_bill(bill,year))))
