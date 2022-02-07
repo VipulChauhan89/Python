f98c33e7a99fc2257cba2a5d6e68ddce295e9b28
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-02-07 18:51:05 +0530

    Update bill.py

--- Bill-generation-of-mangoes/bill.py
+++ Bill-generation-of-mangoes/bill.py
@@ -5,12 +5,12 @@
 '''
 
 def total_bill(a,b):
-      if(b>3 and a>600):
-         return (a*88)/100.0;
-      elif(b>3 and a<=600):
-         return (a*93)/100.0;
-      else:
-         return a;
+   if(b>3 and a>600):
+      return (a*88)/100.0;
+   elif(b>3 and a<=600):
+      return (a*93)/100.0;
+   else:
+      return a;
 
 bill=float(input("Enter the bill amount : "))
 year=float(input("Enter the number of year he is known to the seller : "))
