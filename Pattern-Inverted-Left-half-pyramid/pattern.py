3a2fe263ef0637eafb028a7c74e6de9906194cc5
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-02-11 18:15:38 +0530

    Create pattern.py

---
+++ Pattern-Inverted-Left-half-pyramid/pattern.py
@@ -0,0 +1,17 @@
+'''
+    Program to print the pattern of Inverted Left half pyramid
+    * * * * *
+    * * * *
+    * * *
+    * *
+    * 
+'''
+
+n=int(input("Enter the value of n to print the pattern : "))
+for i in range(0,n):
+    for j in range(0,n):
+        if(i+j<=n-1):
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
