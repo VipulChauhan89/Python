61007408f262c5edc660377177924d1c3058af3f
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-04-09 22:13:25 +0530

    Create Armstrong.py

--- To-check-if-the-number-is-armstrong-number-or-not/Armstrong.py
+++ To-check-if-the-number-is-armstrong-number-or-not/Armstrong.py
@@ -1,13 +1,16 @@
 #Program to check if the number is armstrong number or not
 print("Enter the number : ")
 num=int(input())
-temp=num
-sum=0
-while(temp!=0):
-    s=temp%10
-    sum=sum+(s**3)
-    temp=temp//10
-if(sum==num):
-    print("{} is an armstrong number.\n".format(num));
+if(num>=100 and num<=999):
+    temp=num
+    sum=0
+    while(temp!=0):
+        s=temp%10
+        sum=sum+(s**3)
+        temp=temp//10
+    if(sum==num):
+        print("{} is an armstrong number.\n".format(num));
+    else:
+        print("{} is not an armstrong number.\n".format(num))
 else:
-    print("{} is not an armstrong number.\n".format(num))
+    print("It is not a three digit number.\n")
